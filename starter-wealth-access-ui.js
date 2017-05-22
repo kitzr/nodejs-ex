0d6209052969db2999cfa873f56bc072d8775b56
mquito <michael.quito@macquarie.com>
2017-05-22 10:34:57 +0800

    minimum requirements

--- starter-wealth-access-ui.js
+++ starter-wealth-access-ui.js
@@ -1,6 +1,5 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    fs      = require('fs'),
     app     = express();
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
@@ -10,7 +9,7 @@ var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 app.get('/', function (req, res) {
 
-res.end('Hello Testing 5 remove object assign');
+res.end('Hello Testing package json');
 });
 
 
