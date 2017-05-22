9a43fccce09a524faf7cb92bca9cc1a60675cf3b
mquito <michael.quito@macquarie.com>
2017-05-22 10:23:04 +0800

    minimum requirements

--- starter-wealth-access-ui.js
+++ starter-wealth-access-ui.js
@@ -10,14 +10,14 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
 
 app.get('/', function (req, res) {
 
-res.end('Hello Testing 2');
+res.end('Hello Testing 3');
 });
 
 
